fb599684fc10b0de655dfb7aae3541b7978b0cce
Tsuyoshi Akiyama <ka090824@gmail.com>
2020-11-29 22:04:18 +0900

    feat(model): add vgg16 for transfer learning

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 # Import modules
 import csv
-from generator import generator
 import random
+from generator import generator
 import tensorflow as tf
 from math import ceil
 from sklearn.model_selection import train_test_split
-from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
 from keras.layers import Dense, Lambda, Dropout, Input, GlobalAveragePooling2D, Cropping2D
 from keras.optimizers import Adam
@@ -18,11 +18,16 @@ def get_model():
     train_samples_shape = (160, 320, 3)
     model_input_shape = (96, 96, 3)
 
-    # MobileNetV2 setting
-    mobilenet_v2 = MobileNetV2(weights='imagenet', include_top=False, 
+    # VGG16 setting
+    vgg16 = VGG16(weights='imagenet', include_top=False,
                   input_shape=model_input_shape)
-    mobilenet_v2.layers.pop()
-    mobilenet_v2.trainable = False
+    vgg16.trainable = True
+    for layer in vgg16.layers:
+        if layer.name in ['block5_conv1', 'block4_conv1']:
+            layer.trainable = True
+        else:
+            layer.trainable = False
+    vgg16.summary()
 
     # model input placeholder
     model_inputs_placeholder = Input(shape=train_samples_shape)
@@ -33,9 +38,12 @@ def get_model():
     model_inputs = Lambda(lambda img: preprocess_input(img))(model_inputs)
 
     # For model outputs
-    model_outputs = mobilenet_v2(model_inputs)
+    model_outputs = vgg16(model_inputs)
     model_outputs = GlobalAveragePooling2D()(model_outputs)
-    model_outputs = Dropout(0.3)(model_outputs)
+    model_outputs = Dense(512, activation='relu', kernel_regularizer='l2')(model_outputs)
+    model_outputs = Dropout(0.2)(model_outputs)
+    model_outputs = Dense(512, activation='relu', kernel_regularizer='l2')(model_outputs)
+    model_outputs = Dropout(0.2)(model_outputs)
     model_outputs = Dense(1)(model_outputs)  # Outputs a single value
 
     return Model(inputs=model_inputs_placeholder, outputs=model_outputs)
@@ -49,15 +57,15 @@ if __name__ == "__main__":
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         samples = [line for line in reader]
+        samples = samples[1:]
 
-    # Get rid of 90%, 0 angle images
-    samples = list(filter(lambda line: float(line[3]) != 0.0 or random.uniform(0, 1) > 0.85, samples[1:]))
+    filtered_samples = list(filter(lambda line: float(line[3]) != 0.0 or random.uniform(0, 1) > 0.8, samples))
 
     # Get train and validation samples
-    train_samples, validation_samples = train_test_split(samples, test_size=0.3)
+    train_samples, validation_samples = train_test_split(filtered_samples, test_size=0.2)
 
     # Hyperparam
-    batch_size = 16
+    batch_size = 32
 
     # Coroutine(lazy loading) for train and validation samples
     train_generator = generator(train_samples, batch_size=batch_size, is_augment=True)
@@ -68,7 +76,7 @@ if __name__ == "__main__":
     model.summary()
 
     # Set optimizer and loss function
-    model.compile(loss='mse', optimizer=Adam(3e-3))
+    model.compile(loss='mse', optimizer=Adam(5e-3))
 
     # Callback for saving best result model
     model_checkpoint = ModelCheckpoint(filepath=model_file_path,
@@ -83,6 +91,6 @@ if __name__ == "__main__":
                         steps_per_epoch=ceil(len(train_samples)/batch_size),
                         validation_data=validation_generator,
                         validation_steps=ceil(len(validation_samples)/batch_size),
-                        epochs=1,
+                        epochs=5,
                         verbose=1,
                         callbacks=[model_checkpoint])
